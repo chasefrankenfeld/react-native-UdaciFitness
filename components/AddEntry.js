286a1d3d6c1efe3399823a741cc0ce6fb5ee0156
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2018-02-11 19:06:49 +1100

    Feature: Add TextButton component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UcaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import TextButton from './TextButton';
+import { Ionicons } from '@expo/vector-icons';
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -72,9 +74,32 @@ export default class AddEntry extends Component {
         // Clear local notification
     }
 
+    reset = () => {
+        const key = timeToString()
+
+        // Update Redux
+        // Route to home
+        // Update database
+    }
+
     render() {
 
         const metaInfo = getMetricMetaInfo()
+
+        if ( this.props.alreadyLogged ) {
+            return (
+                <View>
+                    <Ionicons
+                        name='ios-happy-outline'
+                        size={100}
+                    />
+                    <Text>You already logged your information for today</Text>
+                    <TextButton onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
 
         return (
             <View>
