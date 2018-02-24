ab62426606a7b26c03213f4a63b9c7153a3edbfc
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2018-02-24 20:47:16 +1100

    Feature: Add Entry, Submit button, and AsyncStorage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import { 
+    getMetricMetaInfo, 
+    timeToString,
+    getDailyReminderValue
+} from '../utils/helpers';
 import UdaciSlider from './UcaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
 import TextButton from './TextButton';
 import { Ionicons } from '@expo/vector-icons';
+import { submitEntry, removeEntry } from '../utils/api';
+import { connect } from 'react-redux';
+import { addEntry } from '../actions';
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -15,7 +22,7 @@ function SubmitBtn ({ onPress }) {
     )
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
 
     state = {
         run: 0,
@@ -59,7 +66,9 @@ export default class AddEntry extends Component {
         const key = timeToString()
         const entry = this.state
 
-        // Update redux
+        this.props.dispatch(addEntry({
+            [key]: entry
+        }))
 
         this.setState(() => ({
             run: 0,
@@ -70,16 +79,21 @@ export default class AddEntry extends Component {
         }))
 
         // Navigate to home
-        // Save to database
+        
+        submitEntry({ key, entry })
+        
         // Clear local notification
     }
 
     reset = () => {
         const key = timeToString()
 
-        // Update Redux
+        this.props.dispatch(addEntry({
+            [key]: getDailyReminderValue()
+        }))
         // Route to home
-        // Update database
+
+        removeEntry(key)
     }
 
     render() {
@@ -133,3 +147,13 @@ export default class AddEntry extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {
+    const key = timeToString()
+
+    return {
+        alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+    }
+}
+
+export default connect(mapStateToProps)(AddEntry);
