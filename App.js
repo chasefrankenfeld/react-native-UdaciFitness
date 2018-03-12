600e07c8930fcd485dba5a1f2fb0c801ef159e88
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2018-03-12 15:38:41 +1100

    Feature: Add geolocation features

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import reducer from './reducers';
 import History from './components/History';
 import AddEntry from './components/AddEntry';
 import EntryDetail from './components/EntryDetail';
+import Live from "./components/Live";
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { purple, white } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
@@ -32,6 +33,13 @@ const Tabs = TabNavigator({
     navigationOptions: {
       tabBarLable: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLable: "Live",
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
     }
   }
 }, {
